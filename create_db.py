f1bfa5b9a559c6fe681b9dee90868236083dce8c
Nicole Molino <nkm12@duke.edu>
2018-12-02 15:26:36 -0500

    Set up simple server and database

---
+++ create_db.py
@@ -0,0 +1,12 @@
+from pymodm import connect
+from pymodm import MongoModel, fields
+
+connect("mongodb://bme590:hello12345@ds123664.mlab.com:23664/concussion")
+
+# use list field
+class Player(MongoModel):
+    player_id = fields.IntegerField(primary_key=True)
+    session_label = fields.ListField(field=fields.BigIntegerField())
+    session_data = fields.ListField(field=fields.DateTimeField())
+    upload_time = fields.ListField(field=fields.DateTimeField())
+    # session_label and session_data line up by index

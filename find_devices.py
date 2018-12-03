a42724237bfc5635c20e48151985558310d871c4
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-02 20:50:04 -0500

    created function to find all usb devices and print information about them, and a function to print more detailed information from a selected device

--- find_devices.py
+++ find_devices.py
@@ -1,4 +1,34 @@
+import usb
 
 
 def find_devices():
+    busses = usb.busses()
+    for bus in busses:
+        devices = bus.devices
+        for dev in devices:
+            print("Device:", dev.filename)
+            print("  idVendor: %d (0x%04x)" % (dev.idVendor, dev.idVendor))
+            print("  idProduct: %d (0x%04x)" % (dev.idProduct, dev.idProduct))
+            print("Manufacturer:", dev.iManufacturer)
+            print("Serial:", dev.iSerialNumber)
+            print("Product:", dev.iProduct)
     return
+
+
+def get_info():
+    dev = usb.core.find(idVendor=0x05dc, idProduct=0xa81d)  # fill in your own device, of course
+    if dev is None:
+        print('Our device is not connected')
+    else:
+        if dev._manufacturer is None:
+            dev._manufacturer = usb.util.get_string(dev, dev.iManufacturer)
+        print(str(dev._manufacturer))
+        if dev._product is None:
+            dev._product = usb.util.get_string(dev, dev.iProduct)
+        print(str(dev._product))
+    return
+
+
+if __name__ == "__main__":
+    find_devices()
+    get_info()

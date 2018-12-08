abc42ddbac7cf00ed5a936153131a4106cd5d0bb
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-08 14:05:44 -0500

    adds route to send encrypted file to server

--- React_Talk_Server.py
+++ React_Talk_Server.py
@@ -1,16 +1,19 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
+import os
 app = Flask(__name__)
 
 
-@app.route("/api/new_patient", methods=["POST"])
+@app.route("/api/send_env_file", methods=["GET"])
 def send_enc_file():
+    file = '/Test_BIN/L0.BIN'
+    enc = file.encode()
 
-    return
+    return jsonify(enc)
 
 
 if __name__ == "__main__":
 
     # First option is for development
     # Second option is for deployment
-    # app.run(host="127.0.0.1", port=5002)
-    app.run(host="0.0.0.0", port=5002)
+    app.run(host="127.0.0.1", port=5002)
+    # app.run(host="0.0.0.0", port=5002)

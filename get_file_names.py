31d8ce2c3cca394d1f9cc8c0d7ef9f2162866a97
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-09 16:45:01 -0500

    removed time calculations

--- get_file_names.py
+++ get_file_names.py
@@ -7,12 +7,4 @@ def get_file_names(dir):
         for filename in [f for f in filenames if f.endswith(".BIN")]:
             filenames.append(filename)
 
-    times = []
-    for name in filenames:
-        times.append(os.path.getmtime(dir + '/' + name))
-
-    file_info = {
-        'filenames': filenames,
-        'mod_times': times,
-    }
-    return file_info
+    return filenames

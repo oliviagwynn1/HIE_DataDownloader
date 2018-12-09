00f2c8af4e0a9046cc298ee94e1b3b6ba4ee3cad
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-09 16:00:06 -0500

    encoded the binary data in b64 and sent along with hash

--- React_Talk_Server.py
+++ React_Talk_Server.py
@@ -9,18 +9,25 @@ def send_enc_file():
     file = '/Users/clarkbulleit/Desktop/Class Folders/' \
            'Medical Software/Projects/bme590final/Test_BIN/L1.BIN'
 
+    # read file and calculate hash
     hasher = hashlib.md5()
     with open(file, 'rb') as afile:
         dat = afile.read()
         hasher.update(dat)
 
+    # Encode hash for json serializing
     hash = hasher.digest()
     hash_bytes = b64encode(hash)
     hash_str = hash_bytes.decode('utf-8')
 
+    # Encode file for json serializing
+    dat_bytes = b64encode(dat)
+    dat_str = dat_bytes.decode('utf-8')
+
+    #
     test_send = {
         'Hash': hash_str,
-        'File': 2,
+        'File': dat_str,
     }
 
     return jsonify(test_send)

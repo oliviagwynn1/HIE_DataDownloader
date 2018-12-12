0f16082f197fd372c33898659c4aa0098051fd9e
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-12 15:17:48 -0500

    Changed route to post function. This route will now receive a dictionary with the serial numbers the client wishes to download as keys to the mounting point of the device. Then the route will return the JSON serialized information in the correct form for the database

--- React_Talk_Server.py
+++ React_Talk_Server.py
@@ -4,67 +4,84 @@ import hashlib
 from flask_cors import CORS
 from datetime import datetime
 from checksumdir import dirhash
+import requests
 from get_file_names import get_file_names
 from encode_for_json import encode_for_json
 app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/api/send_enc_file", methods=["GET"])
+@app.route("/api/send_enc_file", methods=["POST"])
 def send_enc_file():
-    dir = '/Users/clarkbulleit/Desktop/Class Folders/' \
-           'Medical Software/Projects/bme590final/Test_BIN'
+    """
+    Receives dictionary with the form
+    device_data = {'SN1': '/Volumes/MV1',
+                   'SN2': '/Volumes/MV1 1',
+                   }
+    That contains the serial number and the matched location
+    :return:
+    """
+
+    # dir = '/Users/clarkbulleit/Desktop/Class Folders/' \
+    #        'Medical Software/Projects/bme590final/Test_BIN'
 
     # dir = '/Volumes/MV1'
 
     # Calculate checksum of directory
     # md5hash = dirhash(dir, 'md5')
 
-    # Harvest Filenames from directory
-    filenames = get_file_names(dir)
+    # Get device information dictionary and pull route to device
+    # dir is route to the device, the value in the dict
+    # SN is the serial number, the key in the dictionary
+    device_dict = request.get_json()
+    for k, v in device_dict.items():
+        dir = v
+        SN = k
 
-    # Pull all of the filenames and get a list of dates
-    session_dates = set()
-    for file in filenames:
-        date_stamp = os.path.getmtime(file)
-        session_date = datetime.fromtimestamp(date_stamp).strftime('%Y-%m-%d')
-        session_dates.add(session_date)
+        # Harvest Filenames from directory
+        filenames = get_file_names(dir)
 
-    # Setup output dictionary
-    output_dictionary = {
-        '_id': '2',
-        'session_data': {},
-    }
+        # Pull all of the filenames and get a list of dates
+        session_dates = set()
+        for file in filenames:
+            date_stamp = os.path.getmtime(file)
+            session_date = datetime.fromtimestamp(date_stamp).strftime('%Y-%m-%d')
+            session_dates.add(session_date)
 
-    # Input a session date for each of the session dates pulled
-    for date in session_dates:
-        output_dictionary['session_data'][date] = {}
-
-    # output_dictionary = {}
-    # read files, encode, calculate hash, and put in dictionary
-    for name in filenames:
-        hasher = hashlib.md5()
-        with open(name, 'rb') as afile:
-            dat = afile.read()
-            hasher.update(dat)
-
-        hash = hasher.digest()
-        # Encode hash and file for json serializing
-        dat_str = encode_for_json(dat)
-        hash_str = encode_for_json(hash)
-
-        # Get modification and creation timestamp and convert to datetime
-        tm_stamp = os.path.getmtime(name)
-        mod_time = datetime.fromtimestamp(tm_stamp).strftime('%Y-%m-%d %H:%M:%S')
-        mod_date = datetime.fromtimestamp(tm_stamp).strftime('%Y-%m-%d')
-
-        # create nested dictionary for each file and add to bit dict
-        file_dict = {
-            'data': dat_str[0:10],
-            'mod_time': mod_time,
-            'hash': hash_str,
+        # Setup output dictionary
+        output_dictionary = {
+            '_id': SN,
+            'session_data': {},
         }
-        output_dictionary['session_data'][mod_date][os.path.basename(name).replace(".BIN", "")] = file_dict
+
+        # Input a session date for each of the session dates pulled
+        for date in session_dates:
+            output_dictionary['session_data'][date] = {}
+
+        # read files, encode, calculate hash, and put in dictionary
+        for name in filenames:
+            hasher = hashlib.md5()
+            with open(name, 'rb') as afile:
+                dat = afile.read()
+                hasher.update(dat)
+
+            hash = hasher.digest()
+            # Encode hash and file for json serializing
+            dat_str = encode_for_json(dat)
+            hash_str = encode_for_json(hash)
+
+            # Get modification and creation timestamp and convert to datetime
+            tm_stamp = os.path.getmtime(name)
+            mod_time = datetime.fromtimestamp(tm_stamp).strftime('%Y-%m-%d %H:%M:%S')
+            mod_date = datetime.fromtimestamp(tm_stamp).strftime('%Y-%m-%d')
+
+            # create nested dictionary for each file and add to bit dict
+            file_dict = {
+                'data': dat_str[0:10],
+                'mod_time': mod_time,
+                'hash': hash_str,
+            }
+            output_dictionary['session_data'][mod_date][os.path.basename(name).replace(".BIN", "")] = file_dict
 
     return jsonify(output_dictionary)
 

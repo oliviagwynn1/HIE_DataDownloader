c9c3215445c8092be0b87c4399576a7b5e3e3429
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-12-12 18:43:20 -0500

    change get request to clark's server

--- front-end/src/home.js
+++ front-end/src/home.js
@@ -8,24 +8,23 @@ import ErrorMessage from './error';
 
 class Home extends Component {
     state = {
-        'loading': false,
-        'player_id': [],
+        'data':[] ,
         'errorMessageOpen': false,
     };
 
 
     connectDevices = () => {
-        let get_connectDevices = axios.get('http://127.0.0.1:5000/name');
+        let get_connectDevices = axios.get('http://vcm-7335.vm.duke.edu:5005/api/send_device_info');
         get_connectDevices.then( (data) => {
-            this.setState({'player_id': [data.data.player_id]})
+            this.setState({'data': data.data})
             if (data.data.status > 300) {
                 console.log("Issue initializing device");
                 this.setState({'errorMessageOpen': true})
             }
             else {
                 this.props.view()
-                console.log([data.data.player_id])
-                console.log(data)
+                console.log(this.state.data.Num_Files)
+                console.log(data.data.Players)
                 console.log(this.props.view)
             }
         })

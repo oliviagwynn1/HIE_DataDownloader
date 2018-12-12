15934983c8919960ed2071c7f95d56646b17170e
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-11 20:50:41 -0500

    changed check_hash function to work with new data structure

--- check_hash.py
+++ check_hash.py
@@ -5,35 +5,49 @@ from encode_for_json import encode_for_json
 
 
 if __name__ == "__main__":
-    # r = requests.get('http://127.0.0.1:5005/api/send_enc_file')
-    r = requests.get('http://vcm-7335.vm.duke.edu:5004/api/send_enc_file')
+    r = requests.get('http://127.0.0.1:5005/api/send_enc_file')
+    # r = requests.get('http://vcm-7335.vm.duke.edu:5004/api/send_enc_file')
     data = r.json()
 
-    # Sets up one list for the harvested hashes, another for the calculated ones
-    hashes = []
-    newhashes = []
+    # Create list of time keys Sent:
+    session_times = []
+    session_data = data['Sessions']
+    for keys, values in session_data.items():
+        session_times.append(keys)
 
-    # Iterate through the meta-dictionary
-    for k, v in data.items():
-        hasher = hashlib.md5()
+    # Iterate through each session date sent
+    for time in session_times:
+        date_data = session_data[time]
+        hashes = []
+        newhashes = []
 
-        # Iterate through all sub-dictionaries
-        if isinstance(v, dict):
+        # Iterate through the meta-dictionary
+        for k, v in date_data.items():
+            hasher = hashlib.md5()
 
-            # Save sent hashes in list
-            hashes.append(v['hash'])
-            # Decode the data
-            data_binary = b64decode(v['data'])
-            # Write to local file
-            # calculate hash of decoded data
-            hasher.update(data_binary)
-            # Digest Hash and save as JSON encoded form
-            hash = hasher.digest()
-            hash_str = encode_for_json(hash)
-            # Add newly calculated hash to newhash list
-            newhashes.append(hash_str)
+            # Iterate through all sub-dictionaries
+            if isinstance(v, dict):
 
-    print(type(hashes[0]))
-    print(newhashes)
-    print(hashes == newhashes)
+                # Save sent hashes in list
+                hashes.append(v['hash'])
+                # Decode the data
+                data_binary = b64decode(v['data'])
+                # Write to local file
+                # calculate hash of decoded data
+                hasher.update(data_binary)
+                # Digest Hash and save as JSON encoded form
+                hash = hasher.digest()
+                hash_str = encode_for_json(hash)
+                # Add newly calculated hash to newhash list
+                newhashes.append(hash_str)
+
+        if hashes == newhashes:
+            print('All data for session date {0} was received properly'.format(time))
+        else:
+            print('Data on {0} was not received properly'.format(time))
+
+
+    # print(hashes)
+    # print(newhashes)
+    # print(hashes == newhashes)
 

ffd906d8e54c9b253c21e546bc752ccb9e20de95
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-12 21:24:07 -0500

    fixed get_serial_number function by reading the entire file. Readlines was having problems

--- get_serial_numbers.py
+++ get_serial_numbers.py
@@ -3,7 +3,7 @@
 def get_serial_numbers(path):
 
     file = open(path, 'rb')
-    header_block = file.readline(512)
+    header_block = file.read()
     sn_bytes = header_block[416:420]
     sn = int.from_bytes(sn_bytes,  byteorder='little')
 

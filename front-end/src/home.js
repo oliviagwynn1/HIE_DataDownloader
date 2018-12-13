f89f79d257088c242353074fe65271b35b95297c
Rebecca Cohen <rebecca.cohen@duke.edu>
2018-12-12 21:00:22 -0500

    Managed data recieved from backend

--- front-end/src/home.js
+++ front-end/src/home.js
@@ -8,24 +8,20 @@ import ErrorMessage from './error';
 
 class Home extends Component {
     state = {
-        'data':[] ,
         'errorMessageOpen': false,
     };
 
 
     connectDevices = () => {
         let get_connectDevices = axios.get('http://vcm-7335.vm.duke.edu:5005/api/send_device_info');
-        get_connectDevices.then( (data) => {
-            this.setState({'data': data.data})
-            if (data.data.status > 300) {
+        get_connectDevices.then( (response) => {
+            this.props.data(response);
+            if (response.data.status > 300) {
                 console.log("Issue initializing device");
                 this.setState({'errorMessageOpen': true})
             }
             else {
                 this.props.view()
-                console.log(this.state.data.Num_Files)
-                console.log(data.data.Players)
-                console.log(this.props.view)
             }
         })
         .catch( (error) => {

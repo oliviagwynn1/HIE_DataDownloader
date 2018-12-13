189693af9607fff3fd27329610df35b6e074036f
Clark Bulleit <clark.bulleit@duke.edu>
2018-12-12 21:24:58 -0500

    added testing functions

--- test_get_serial_numbers.py
+++ test_get_serial_numbers.py
@@ -7,6 +7,10 @@ import pytest
     ('0/L2.BIN', 261758686),
     ('0/L3.BIN', 261758686),
     ('0/L4.BIN', 261758686),
+    ('100/L100.BIN', 261758686),
+    ('100/L105.BIN', 261758686),
+    ('100/L107.BIN', 261758686),
+    ('100/L101.BIN', 261758686),
 ])
 def test_get_serial_numbers(a, expected):
     path = '/Users/clarkbulleit/Desktop/Class Folders/' \
